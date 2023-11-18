4f425717448e7412ed47030d06f45875d3590fcf
deuk9 <cdy900@gmail.com>
2023-11-18 12:44:27 +0900

    draft

---
+++ src/main/java/example/test/member/controller/MemberController.java
@@ -0,0 +1,16 @@
+package example.test.member.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+
+    @PostMapping
+    public void register()
+
+
+}
---
+++ src/main/java/example/test/member/model/Member.java
@@ -0,0 +1,21 @@
+package example.test.member.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+
+}
